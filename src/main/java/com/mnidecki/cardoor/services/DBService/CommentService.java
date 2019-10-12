109edc37452fadbd480a670d1e08003eb6c1be01
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-10-12 12:26:48 +0200

    car brand and model default value implementation

--- src/main/java/com/mnidecki/cardoor/services/DBService/CommentService.java
+++ src/main/java/com/mnidecki/cardoor/services/DBService/CommentService.java
@@ -1,6 +1,7 @@
 package com.mnidecki.cardoor.services.DBService;
 
 import com.mnidecki.cardoor.domain.car.Comment;
+import com.mnidecki.cardoor.domain.car.Star;
 import com.mnidecki.cardoor.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,9 @@ public class CommentService {
     private CommentRepository commentRepository;
     @Autowired
     private UserService userService;
+    @Autowired
+    private StarService starService;
+
 
     public List<Comment> findAll() {
         return commentRepository.findAll();
@@ -26,8 +30,19 @@ public class CommentService {
     }
 
     public Comment save(final Comment comment) {
+        List<Comment> commentList = findAllByModel_Id(comment.getModel().getId());
+        commentList.add(comment);
+
+        int sum = commentList.stream().mapToInt(Comment::getRating).sum();
+        float rating = sum/ (float) commentList.size();
+
+        Star star = starService.findById(comment.getModel().getId());
+        star.setRatingAverage(rating);
+        starService.save(star);
+
         comment.setUser(userService.getUserFromAuthentication());
         comment.setCreationDate(Timestamp.valueOf(LocalDateTime.now()));
+
         return commentRepository.save(comment);
     }
 
